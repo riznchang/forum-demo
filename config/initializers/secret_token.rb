1b4081e6bccb786d5c9532a4c96920401569b762
Rizn Chang <rizn.chang@gmail.com>
2011-11-24 17:23:23 +0800

    Reason Forum.5.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '682d5a8fd034d61bb72d969f1d48c7417e794618bc938142f3ae9579f41d6a005e913d0c697e624b356104936efe32191cfa30c4b3f4f7f83b62b59679742b81'
